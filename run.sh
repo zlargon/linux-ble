5b7b95fb83f5a3bcf2ec78a5202c27d2ff5ebfcb
zlargon <zlargon1988@gmail.com>
2019-09-30 10:41:53 -0400

    update run script

--- run.sh
+++ run.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-
 # sudo apt install libbluetooth-dev
 
-rm -rf *.exe
+# 1. clean
+echo -e "\n[x] Clean"
+rm -rvf *.exe
 
-# build
-gcc -o main.exe main.c -lbluetooth
+# 2. select source
+if [ "${1}" == "" ]; then
+    src="main.c"
+else
+    src=${1}
+fi
 
-# run executable
-./main.exe
+# 3. build
+echo -e "\n[x] Build ${src}"
+gcc -o ${src}.exe ${src} -lbluetooth
+
+# 4. run
+echo -e "\n[x] Run ${src}"
+./${src}.exe
